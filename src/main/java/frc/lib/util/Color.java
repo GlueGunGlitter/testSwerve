1a9df9a8d0a59f836d9abd755270315a9fd2be85
Ofek-Z <ofrkzarda1212@gmail.com>
2023-02-06 18:49:26 +0200

    plus files

---
+++ src/main/java/frc/lib/util/Color.java
@@ -0,0 +1,14 @@
+package frc.lib.util;
+
+public enum Color {
+
+    Red,
+    Blue,
+    Green,
+    Yellow,
+    Pink,
+    Purple,
+    Orange,
+    White,
+    Black
+}
---
+++ src/main/java/frc/robot/subsystems/ColorNameToRGB.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import java.util.Map;
+
+import frc.lib.util.Color;
+
+
+public class ColorNameToRGB {
+
+    static final Map<Enum<Color>, float[]> COLORS = Map.of(
+            Color.Red, Constants.RED_RGB,
+            Color.Blue, Constants.BLUE_RGB,
+            Color.Green, Constants.GREEN_RGB,
+            Color.Yellow, Constants.YELLOW_RGB,
+            Color.Pink, Constants.PINK_RGB,
+            Color.Purple, Constants.PURPLE_RGB,
+            Color.White, Constants.WHITE_RGB,
+            Color.Black, Constants.BLACK_RGB,
+            Color.Orange, Constants.ORANGE_RGB
+    );
+
+    public static float[] colorToRGB(Color color) {
+        return COLORS.get(color);
+    }
+}
