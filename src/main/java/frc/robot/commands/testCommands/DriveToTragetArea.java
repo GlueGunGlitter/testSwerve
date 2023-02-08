12a487d0502246b8fd19922c759a4df0798e3c9c
Gabriel Bogdanovsky <gabi0526287131@gmail.com>
2023-02-08 09:48:05 +0200

    fix drive to target area

--- src/main/java/frc/robot/commands/testCommands/DriveToTragetArea.java
+++ src/main/java/frc/robot/commands/testCommands/DriveToTragetArea.java
@@ -54,7 +54,14 @@ public class DriveToTragetArea extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    swerve.drive(
+      new Translation2d(0, 0).times(Constants.Swerve.maxSpeed), 
+      0 * Constants.Swerve.maxAngularVelocity, 
+      true, 
+      true
+    );
+  }
 
   // Returns true when the command should end.
   @Override
