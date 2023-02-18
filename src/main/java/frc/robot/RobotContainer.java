5ca4f1d67c212b6cd0ef69a6689de7170ff80185
Gabriel Bogdanovsky <gabi0526287131@gmail.com>
2023-02-18 12:41:43 +0200

    .

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -122,7 +122,7 @@ public class RobotContainer {
 
         hkB.onTrue(new placemid(m_ARM, m_grapper));
 
-        hka.onTrue(Commands.runOnce(m_ARM.setposison(5)));
+        hka.onTrue(Commands.runOnce(() -> m_ARM.setposison(5)));
 
         hkRB.onTrue(new ResetArm(m_ARM));
 
